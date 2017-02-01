65175cfc0ff9d9016de64e5a88bd46ab1c5fb091
Adam Ruf <adam@adamruf.com>
2017-01-31 21:14:48 -0800

    Added index and workday calculator script

---
+++ js/workdays-calculator.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	$('.js-calculate').click(function(){
+  var d1 = $('.js-from-date').val();
+  var d2 = $('.js-to-date').val();
+		$('.js-result').text(workingDaysBetweenDates(d1,d2));
+	});
+});
+
+function workingDaysBetweenDates(d0, d1) {
+	var holidays = ['2017-02-20','2017-05-29', '2017-07-04', '2017-09-04', '2017-10-09', '2017-11-23', '2017-12-25'];
+    var startDate = parseDate(d0);
+    var endDate = parseDate(d1);
+    // Validate input
+    if (endDate < startDate) {
+        return 'Sorry, this calculator only works if the FROM date is earlier than the TO date';
+    }
+    // Calculate days between dates
+    var millisecondsPerDay = 86400 * 1000; // Day in milliseconds
+    startDate.setHours(0,0,0,1);  // Start just after midnight
+    endDate.setHours(23,59,59,999);  // End just before midnight
+    var diff = endDate - startDate;  // Milliseconds between datetime objects
+    var days = Math.ceil(diff / millisecondsPerDay);
+
+    // Subtract two weekend days for every week in between
+    var weeks = Math.floor(days / 7);
+    days -= weeks * 2;
+
+    // Handle special cases
+    var startDay = startDate.getDay();
+    var endDay = endDate.getDay();
+
+    // Remove weekend not previously removed.
+    if (startDay - endDay > 1) {
+        days -= 2;
+    }
+    // Remove start day if span starts on Sunday but ends before Saturday
+    if (startDay === 0 && endDay !== 6) {
+        days--;
+    }
+    // Remove end day if span ends on Saturday but starts after Sunday
+    if (endDay === 6 && startDay !== 0) {
+        days--;
+    }
+    /* Here is the code */
+    for (var i in holidays) {
+      if ((holidays[i] >= d0) && (holidays[i] <= d1)) {
+      	days--;
+      }
+    }
+    return days;
+}
+
+function parseDate(input) {
+	// Transform date from text to date
+  var parts = input.match(/(\d+)/g);
+  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+}
